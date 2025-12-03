5d4b343a4c86086a848b5c7d96bf0b659f3ea9c6
Atto9752 <anto.faundes@duocuc.cl>
2025-12-03 16:16:00 -0300

    avance en controller

--- admin/src/main/java/com/example/admin/controller/adminControlador.java
+++ admin/src/main/java/com/example/admin/controller/adminControlador.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
 import com.example.admin.model.dto.admin;
 import com.example.admin.service.adminService;
 
@@ -17,22 +22,27 @@ public class adminControlador {
     @Autowired
     private adminService servicio;
 
+    @GetMapping
     public List<admin> listarAdmins() {
         return servicio.listarAdmins();
     }
 
+    @PostMapping
     public admin guardarAdmin(admin admin) {
         return servicio.guardarAdmin(admin);
     }
 
+    @DeleteMapping("/{id}")
     public void eliminarAdmin(int id) {
         servicio.eliminarAdmin(id);
     }
 
+    @GetMapping("/{id}")
     public admin buscarAdmin(int id) {
         return servicio.buscarAdmin(id);
     }
 
+    @PutMapping("/{id}")
     public admin actualizarAdmin(int id, admin adminActualizado) {
         return servicio.actualizarAdmin(id, adminActualizado);
     }
